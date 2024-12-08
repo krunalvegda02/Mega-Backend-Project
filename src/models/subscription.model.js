8c1c27fd3cffeaba7aa304085b4cbec7c3a5cacd
krunalvegda02 <Krunal.vegda02@gmail.com>
2024-12-08 14:09:16 +0530

    Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId, //one who is subscribing
+      ref: "User",
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId, //one to whom 'Subscriber' is subscribing
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
